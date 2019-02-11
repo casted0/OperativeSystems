a62d8e9a3726ae1ee0b778147d4d4038f9229d54
Kasteo <pablo.castedo@estudiante.uam.es>
2019-02-11 12:52:45 +0100

    xd

--- Practica 1/p1_exercise_4.c
+++ Practica 1/p1_exercise_4.c
@@ -9,26 +9,35 @@
 int main(void)
 {
 	pid_t pid;
-	int i;
-	for(i = 0; i < NUM_PROC; i++)
+	int i=0;
+
+	pid = fork();
+	if(pid <  0)
 	{
-		pid = fork();
-		if(pid <  0)
-		{
-			printf("Error al emplear fork\n");
-			exit(EXIT_FAILURE);
-		}
-		else if(pid ==  0)
-		{
-			printf("HIJO pid: %d, ppid: %d\n", getpid(), getppid());
-			exit(EXIT_SUCCESS);
-		}
-		else if(pid > 0)
-		{
-			printf("PADRE %d\n", i);
-			wait(NULL);
+		printf("Error al emplear fork\n");
+		exit(EXIT_FAILURE);
+	}
+	else if(pid ==  0)
+	{
+		printf("HIJO [id]: [%d]\n", i);
+		for(i = 0; i < NUM_PROC; i++){
+			pid = fork();
+			if(pid < 0)
+			{
+				printf("Error al emplear fork\n");
+				exit(EXIT_FAILURE);
+			}else if(pid ==  0)
+			{
+				printf("HIJO [id]: [%d]\n", i);
+			}
 		}
 	}
+	else if(pid > 0)
+	{
+		printf("PADRE %d\n", i);
+		wait(NULL);
+	}
+
 	wait(NULL);
 	exit(EXIT_SUCCESS);
 }
@@ -38,8 +47,8 @@ int main(void)
 a) El motivo de que el código del ejercicio 3 genere hijos que se quedan huérfanos, es que el padre no dispone de un wait() para 
    cada hijo, por lo tanto se puede dar el caso de que el proceso padre finalize antes de que lo haga el hijo
 
-b) 
+b) Introducido wait(NULL en el p1_exercise_3.c)
 
-
+c) 
 
 */
