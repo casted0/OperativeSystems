486790d28ab05a08f4cf9681a797db08c9ce3a09
CarloxA49 <carloxarranz@gmail.com>
2019-03-18 19:28:06 +0100

    Ejer8 a medias

--- Practica 2/Ejercicios/ejercicio8.c
+++ Practica 2/Ejercicios/ejercicio8.c
@@ -7,71 +7,116 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
-#define SEM "/example_sem"
-
 #define N_READ 5
 #define SECS 3
 
-	Lectura() {
-		Down(sem_lectura);
+
+	void manejador(int sig){
+    	printf("He conseguido capturar SIGKILL \n");
+    	fflush(stdout);
+    	exit(EXIT_SUCCESS);
+	}
+
+
+
+	void Lectura(sem_t * sem_lectura, sem_t * sem_escritura) {
+		int lectores=0;
+
+		sem_wait(sem_lectura);
 		lectores++;
 		if (lectores == 1)
-			Down(sem_escritura);
-		Up(sem_lectura);
+			sem_wait(sem_escritura);
+		sem_post(sem_lectura);
 		
-		Leer();
+		/*--------LEER------------*/
+		printf("\nR-INI %d",getpid());
+		sleep(1);
+		printf("\nR-FIN %d",getpid());
+		sleep(SECS);
+		/*--------FIN LEER------------*/
 
-		Down(sem_lectura);
+
+		sem_wait(sem_lectura);
 		lectores--;
 		if (lectores == 0)
-			Up(sem_escritura);
-		Up(sem_lectura);
+			sem_post(sem_escritura);
+		sem_post(sem_lectura);
 
 	}
 
 
-	Escritura() {
-		Down(sem_escritura);
+	void Escritura(sem_t * sem_escritura) {
+		sem_wait(sem_escritura);
 
-		Escribir();
+		/*--------ESCRIBIR------------*/
+		printf("W-INI %d",getpid());
+		sleep(1);
+		printf("W-FIN %d",getpid());
 
-		Up(sem_escritura);
+		sleep(SECS);
+
+		/*--------FIN ESCRIBIR-----------*/
+
+		sem_post(sem_escritura);
 	}
 
 	int main(){
 
 		int i,pid;
+		sem_t * sem_lectura=NULL;
+		sem_t * sem_escritura=NULL;
+
+		if ((sem_lectura = sem_open("/SEM_LECT", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, N_READ)) == SEM_FAILED) {
+			perror("sem_open");
+			exit(EXIT_FAILURE);
+		}
+
+		if ((sem_escritura = sem_open("/SEM_ESCRIT", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1)) == SEM_FAILED) {
+			perror("sem_open");
+			exit(EXIT_FAILURE);
+		}
+
+
+		struct sigaction act;
+    	act.sa_handler = manejador;
+    	sigemptyset(&(act.sa_mask));
+    	act.sa_flags =0;
+
+ 		if(sigaction(SIGINT,&act,NULL)<0){
+        	perror("sigaction");
+        	exit(EXIT_FAILURE);
+    	}
 
 		for(i=0;i<N_READ;i++){
 
-			if(!pid=fork()){
+			if(!(pid=fork())){
 				break;
 			}
 		}
 
 		if(!pid){
-			
-			while(true){
-			
-				printf("\nR-INI %d",getpid());
-				sleep(1);
-				printf("\nR-FIN %d",getpid());
+			//hijos lectores
+			while(1){
 
-				sleep(SECS);
+				Lectura(sem_lectura,sem_escritura);
+				
 			}
+
 
 		}
 		else{
+			//padre escritor
+			while(1){
 
-			while(true){
 
-				printf("W-INI %d",getpid());
-				sleep(1);
-				printf("W-FIN %d",getpid());
+				Escritura(sem_escritura);
 
-				sleep(SECS);
 			}
 		}
+
+		//sem_unlink(sem_escritura);
+		//sem_unlink(sem_lectura);
+
+		return 0;
 
 	}
